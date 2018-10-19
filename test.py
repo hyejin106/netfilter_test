ed8de26bb1592147e1c8a1507d4846a82d84e9fa
hyejin106 <hyejinj413@gmail.com>
2018-10-20 00:42:34 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 def IterateLengthOfParam(self, msg, param_value, opcode)
+
 def IterateOPcode(self, msg)
+
 def RandomizeParameter(self, msg, opcode, num, blc_num)
+
 def IterateMSGcodeWithBigBlock(self, msg, opcode, param_value, blc_size)

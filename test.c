95ebe4932a59d4e1ac1b817326c465ed1e8de9aa
hyejin106 <hyejinj413@gmail.com>
2018-10-20 00:43:31 +0900

    Create test.c

---
+++ test.c
@@ -0,0 +1,6 @@
+byte onebitFlip(byte val);
+byte nbitFlip(byte val);
+byte SwapRandomBit(byte val);
+byte ShiftBitToLeft(byte val);
+void ChangeToFF(byte *data, int idx,int len);
+void CopyValue(byte *data, int idx, int len, int is_edid);
